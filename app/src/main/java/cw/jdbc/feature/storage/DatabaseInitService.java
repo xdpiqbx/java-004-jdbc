94f33b0ebbf005cf1d5a062965e4ea1ef84fbf3e
xdpiqbx <xdpiqbx@gmail.com>
2022-11-30 07:41:49 +0200

    in proc

---
+++ app/src/main/java/cw/jdbc/feature/storage/DatabaseInitService.java
@@ -0,0 +1,19 @@
+package cw.jdbc.feature.storage;
+
+import cw.jdbc.feature.prefs.Prefs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DatabaseInitService {
+    public void initDb(Storage storage){
+        String initDbFilename = new Prefs().getString(Prefs.INIT_DB_SQL_FILE_PATH);
+        try {
+            String sql = String.join("\n", Files.readAllLines(Paths.get(initDbFilename)));
+            storage.executeUpdate(sql);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
